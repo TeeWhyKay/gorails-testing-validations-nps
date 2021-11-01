5c0d880051ebb7fe505c979039cf44705541ff67
YongKeeTan <yongkee.t@gmail.com>
2021-11-01 12:45:33 +0800

    validation for rating outside limits

--- test/models/rating_test.rb
+++ test/models/rating_test.rb
@@ -4,4 +4,14 @@ class RatingTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'invalid if score is less than 0' do
+    rating = Rating.new(score: -1)
+    rating.valid?
+    assert_not rating.errors[:score].empty?
+  end
+  test 'invalid if score is more than 10' do
+    rating = Rating.new(score: 11)
+    rating.valid?
+    assert_not rating.errors[:score].empty?
+  end
 end
