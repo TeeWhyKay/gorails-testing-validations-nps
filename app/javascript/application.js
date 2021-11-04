f63ec6fae5a04ec29ec13423088d890441f12f59
YongKeeTan <yongkee.t@gmail.com>
2021-11-04 22:55:28 +0800

    fixing rails asset pipeline

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,9 @@
 // Entry point for the build script in your package.json
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
 import "./controllers"
 import "@hotwired/turbo-rails"
 import * as bootstrap from "bootstrap"
