1e15facfd0a2671282c5a5eadc18baa5478b4a08
YongKeeTan <yongkee.t@gmail.com>
2021-11-05 12:45:07 +0800

    added destroy test

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -60,58 +60,13 @@ class ProjectsControllerTest < ActionDispatch::IntegrationTest
     patch project_url(@project), params: { project: { title: '' } }
     # when update and create fails it returns the form with 200 due to render
     # you could assert select the error message eg. 'Invalid title name'
-    assert_response :success
+    assert_response :unprocessable_entity
   end
 
-
-  # this test is a redundant test.. as it is duplicated.
-  # test 'can get new' do
-  #   get new_project_url
-  #   assert_response :redirect
-  # end
-  # setup do
-  #   @project = projects(:one)
-  # end
-
-  # test "should get index" do
-  #   get projects_url
-  #   # making sure http 200
-  #   assert_response :success
-  # end
-
-  # test "should get new" do
-  #   get new_project_url
-  #   assert_response :success
-  # end
-
-  # test "should create project" do
-  #   assert_difference('Project.count') do
-  #     post projects_url, params: { project: { title: @project.title } }
-  #   end
-
-  #   assert_redirected_to project_url(Project.last)
-  # end
-
-  # test "should show project" do
-  #   get project_url(@project)
-  #   assert_response :success
-  # end
-
-  # test "should get edit" do
-  #   get edit_project_url(@project)
-  #   assert_response :success
-  # end
-
-  # test "should update project" do
-  #   patch project_url(@project), params: { project: { title: @project.title } }
-  #   assert_redirected_to project_url(@project)
-  # end
-
-  # test "should destroy project" do
-  #   assert_difference('Project.count', -1) do
-  #     delete project_url(@project)
-  #   end
-
-  #   assert_redirected_to projects_url
-  # end
+  test 'can destroy project' do
+    assert_difference('Project.count', -1) do
+      delete project_url(@project)
+    end
+    assert_redirected_to projects_url
+  end
 end
