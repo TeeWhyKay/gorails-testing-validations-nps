6dd5d813e734fc4b2f16c7aba9ac0499e1e2ef99
YongKeeTan <yongkee.t@gmail.com>
2021-11-08 12:29:57 +0800

    test: can update a project

--- test/system/projects_test.rb
+++ test/system/projects_test.rb
@@ -1,25 +1,37 @@
 require "application_system_test_case"
 
 class ProjectsTest < ApplicationSystemTestCase
+  setup do
+    @project = projects(:one)
+  end
+
   test 'visiting the index' do
     sign_in users(:regular)
     visit projects_url
     assert_selector 'h1', text: 'Projects'
   end
 
-  # test 'can create a project' do
-  #   sign_in users(:regular)
-  #   visit projects_url
+  test 'can create a project' do
+    sign_in users(:regular)
+    visit projects_url
 
-  #   click_on 'Add New Project'
-  #   fill_in 'Title', with: 'Hello Test'
-  #   click_on 'Create Project'
+    click_on 'Add New Project'
+    fill_in 'Title', with: 'Hello Test'
+    click_on 'Create Project'
 
-  #   assert_selector 'h1', text: 'Hello Test'
-  # end
-  # setup do
-  #   @project = projects(:one)
-  # end
+    assert_selector 'h1', text: 'Hello Test'
+  end
+
+  test 'can update a project' do
+    sign_in users(:regular)
+    visit project_url(@project)
+
+    click_on 'Edit'
+    fill_in 'Title', with: 'Hello Test'
+    click_on 'Update Project'
+
+    assert_selector 'h1', text: 'Hello Test'
+  end
 
   # test "visiting the index" do
   #   visit projects_url
