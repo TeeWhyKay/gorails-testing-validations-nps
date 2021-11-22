864df062cd095973e3c018c3bf5f8d5203ff7ea9
YongKeeTan <yongkee.t@gmail.com>
2021-11-22 12:58:52 +0800

    remove peoples table migration confusing rails

--- db/migrate/20211105040631_change_people_to_peoples.rb
+++
@@ -1,5 +0,0 @@
-class ChangePeopleToPeoples < ActiveRecord::Migration[6.0]
-  def change
-    rename_table :people, :peoples
-  end
-end
--- db/migrate/20211122032953_remove_rating_index_from_people.rb
+++
@@ -1,5 +0,0 @@
-class RemoveRatingIndexFromPeople < ActiveRecord::Migration[6.0]
-  def change
-    remove_index :people, :rating_id
-  end
-end
